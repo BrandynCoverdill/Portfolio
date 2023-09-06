b35779ee7f4b7a33f60760e5511be360905a4e42
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2023-09-05 23:06:29 -0600

    Added email validation and made it so the email does go through securely.

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const form = document.querySelector('form');
+const submitBtn = document.getElementById('submit');
+
+/**
+ * Checks if the form is validated before sending.
+ */
+submitBtn.addEventListener('click', (e) => {
+    // Scoped variables
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const message = document.getElementById('message');
+    const nameError = document.querySelector('.form-error:nth-of-type(1)');
+    const emailError = document.querySelector('.form-error:nth-of-type(2)');
+    const messageError = document.querySelector('.form-error:nth-of-type(3)');
+    const emailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    
+    // Reset form without error messages
+    name.style.border = "";
+    nameError.style.display = "none";
+    email.style.border = "";
+    emailError.style.display = "none";
+    message.style.border = "";
+    messageError.style.display = "none";
+    
+    // Check if user input is invalidated
+    if (!name.value ?? null) {
+        e.preventDefault();
+        nameError.style.display = "block";
+        name.style.border = "1px solid red";
+    }
+    if (!emailRegex.test(email.value)) {
+        e.preventDefault();
+        emailError.style.display = "block";
+        email.style.border = "1px solid red";
+    }
+    if (!message.value ?? null) {
+        e.preventDefault();
+        messageError.style.display = "block";
+        message.style.border = "1px solid red";
+    }
+});
+
+// Reset form on successful submission.
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName('form')) {
+        form.reset();
+        email.style.border = "none";
+    }
+}
+
+
+
